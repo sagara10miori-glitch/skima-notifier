f9f524cc25d12cce2ec9d02b187b6329a24efa94
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-10 15:53:15 +0900

    Update settings.py
    Signed-off-by: sagara10miori-glitch <sagara10miori@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 # config/settings.py
+import os
 
 PRIORITY_USERS_PATH = "users/priority_users.txt"
 EXCLUDE_USERS_PATH = "users/exclude_users.txt"
 
-WEBHOOK_URL = "WEBHOOK_URL"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
