cf0462b00cda918a206af74829fdcb1d305f743e
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-19 00:14:59 +0900

    Update utils.py
    Signed-off-by: sagara10miori-glitch <sagara10miori@gmail.com>

--- utils.py
+++ utils.py
@@ -4,17 +4,10 @@ from json import JSONDecodeError
 
 
 def load_user_list(path):
-    """
-    JSON 配列（["123", "456"]）を読み込んで set にして返す。
-    - ファイルが無い場合は空集合
-    - JSON が壊れている場合も空集合
-    - 配列以外（dict など）は無視
-    """
     try:
         with open(path, "r") as f:
             data = json.load(f)
 
-        # 配列以外は無視
         if isinstance(data, list):
             return set(data)
         return set()
@@ -24,10 +17,6 @@ def load_user_list(path):
 
 
 def normalize_price(text):
-    """
-    数字以外を除去して int に変換。
-    空文字や None の場合は 0 を返す。
-    """
     if not text:
         return 0
 
