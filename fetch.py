b3a61c09a89ade6444794389858171ad32a64b24
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-19 22:24:53 +0900

    Update fetch.py
    Signed-off-by: sagara10miori-glitch <sagara10miori@gmail.com>

--- fetch.py
+++ fetch.py
@@ -10,6 +10,15 @@ session = cloudscraper.create_scraper(
         "mobile": False
     }
 )
+
+session.headers.update({
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    )
+})
+
 URL = "https://skima.jp/item-list"
 
 
