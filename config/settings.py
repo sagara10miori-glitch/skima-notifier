6812ed9ba665dc071fd280427626125b920df65c
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-24 16:39:38 +0900

    Update settings.py
    Signed-off-by: sagara10miori-glitch <sagara10miori@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,9 @@
-# config/settings.py
 import os
 
 PRIORITY_USERS_PATH = "users/priority_users.txt"
 EXCLUDE_USERS_PATH = "users/exclude_users.txt"
+
+# 価格上限（通知しない上限）
+PRICE_LIMIT = 999999  # 好きな値に変更可能
 
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
